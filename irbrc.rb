bd83a1e209f4d97c385fe361eb62477ecc77f173
David Runger <davidjrunger@gmail.com>
2022-10-16 19:50:26 -0500

    Add note about IRB not loading `~/.irbrc.rb` because of the extension
    (`~/.irbrc.rb`, formerly `~/.irbrc`, is symlinked to this file.)

--- irbrc.rb
+++ irbrc.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# NOTE! Because of the `.rb` extension, IRB doesn't automatically load this. That's what we want,
+# because, if IRB does automatically load it, then IRB doesn't respect project-local settings.
+# Instead, create a project-local `.irbrc` file, and in it put `load '/Users/david/.irbrc.rb'`.
+
 def skip!
   $stop_skipping_at = Time.at(Integer(Time.now) + 5)
 end
