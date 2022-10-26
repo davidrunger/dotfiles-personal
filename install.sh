c5c46432e4f459c1ad1ee18d08fb434da0169c48
David Runger <davidjrunger@gmail.com>
2022-10-26 12:09:21 -0500

    [install] Add all symlinks
    Found via this command: `for FILE in $(git ls-files | rg -v
    "^bin/|^guardfiles/|^utils/|lefthook.yml|install.sh"); do echo "--
    $FILE"; find -L ~ -samefile $FILE; echo; done`

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+ln -sf ~/workspace/dotfiles/gitignore_global ~/.gitignore_global
+ln -sf ~/workspace/dotfiles/initializers/z.rb ~/workspace/david_runger/config/initializers/z.rb
+ln -sf ~/workspace/dotfiles/irbrc.rb ~/.irbrc.rb
+ln -sf ~/workspace/dotfiles/pryrc.rb ~/.pryrc
+ln -sf ~/workspace/dotfiles/rspec ~/.rspec
+ln -sf ~/workspace/dotfiles/rubocop.yml ~/.rubocop.yml
 ln -sf ~/workspace/dotfiles/zsh-themes ~/.oh-my-zsh/custom/themes
+ln -sf ~/workspace/dotfiles/zsh-themes/bolso.zsh-theme ~/.oh-my-zsh/custom/themes/bolso.zsh-theme
+ln -sf ~/workspace/dotfiles/zshrc.sh ~/.zshrc
