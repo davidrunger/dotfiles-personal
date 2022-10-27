d043f309e9c26dcf3acd634296e9a922281a49f7
David Runger <davidjrunger@gmail.com>
2022-10-27 10:18:11 -0500

    Move monkeypatches from pryrc and irbrc to monkeypatch_repl.rb

--- pryrc.rb
+++ pryrc.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-require_relative './utils/ruby/copy_utils.rb'
-Object.prepend(CopyUtils)
+require_relative './utils/ruby/monkeypatch_repl.rb'
 
 Pry.config.pager = false
 Pry.config.color = true
@@ -21,89 +20,3 @@ end
 require 'amazing_print'
 # similar to `AmazingPrint.pry!`, but with `=> ` at the beginning
 Pry.print = proc { |output, value| output.puts("=> #{value.ai}") }
-
-# rubocop:disable Style/TopLevelMethodDefinition
-def skip!
-  $stop_skipping_at = Time.at(Integer(Time.now) + 5)
-end
-
-def subl(filename = nil)
-  system("subl #{filename}")
-end
-# rubocop:enable Style/TopLevelMethodDefinition
-
-module MethodSource::MethodExtensions
-  def sl
-    source_location&.map(&:to_s)&.join(':')
-  end
-
-  def s
-    return nil if sl.blank?
-
-    subl(sl)
-  end
-end
-
-class Object
-  def m(method_name)
-    method(method_name)
-  end
-end
-
-class Time
-  def cppa
-    I18n.l(self, format: :long).gsub(/\s+/, ' ').cpp
-  end
-
-  def cppz
-    in_time_zone.cppz
-  end
-end
-
-if defined?(ActiveSupport)
-  class ActiveSupport::TimeWithZone
-    def cppa
-      I18n.l(self, format: :long).gsub(/\s+/, ' ').cpp
-    end
-
-    def cppz
-      utc.iso8601.cpp
-    end
-  end
-end
-
-class String
-  def cpp
-    # copying SQL, e.g. CandidateAssessmentAttempt.select(:id, :remote_challenge_id).to_sql.cpp
-    if start_with?('SELECT')
-      super(tr('"', '')) # remove double quotes (from around table names, etc.)
-    else
-      super(self)
-    end
-  end
-
-  def ccpp
-    camelize.cpp
-  end
-
-  def ucpp
-    underscore.cpp
-  end
-
-  # "Sublime regex (for searching)"
-  def sr
-    underscore.gsub('_', '.?').cpp
-  end
-end
-
-class Numeric
-  def cpp
-    super(self)
-  end
-end
-
-class Array
-  def cpp
-    super(to_json.gsub(',', ', ')[1..-2]) # spaces between commas and drop brackets for SQL
-  end
-end
