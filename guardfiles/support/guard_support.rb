baa07e06e4be09d953244766a0216d2fad14820f
David Runger <davidjrunger@gmail.com>
2022-11-06 13:08:17 -0600

    [gal] Choose which directories to watch depending on type of guard

--- guardfiles/support/guard_support.rb
+++ guardfiles/support/guard_support.rb
@@ -9,9 +9,22 @@ module GuardSupport
   class << self
     extend Memoist
 
+    # rubocop:disable Style/MutableConstant
+    DIRECTORIES_TO_WATCH = {
+      'crystal' => %w[app personal spec src],
+    }
+    DIRECTORIES_TO_WATCH.default_proc = proc { %w[app lib personal spec] }
+    DIRECTORIES_TO_WATCH.freeze
+    # rubocop:enable Style/MutableConstant
+
     memoize \
     def directories_to_watch
-      %w[app lib personal spec].select { File.directory?(_1) }.freeze
+      DIRECTORIES_TO_WATCH[guardfile_type].select { File.directory?(_1) }.freeze
+    end
+
+    memoize \
+    def guardfile_type
+      ENV.fetch('GUARDFILE_TYPE')
     end
   end
 end
