8fff8b0da9d27c3683cb8c3d81cee8a290ff760c
David Runger <davidjrunger@gmail.com>
2022-11-07 18:43:04 -0600

    [install] Don't try to link the whole `zsh/themes/` directory
    It doesn't work well. It interferes with updating oh-my-zsh.

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ ln -sf ~/code/dotfiles/irbrc.rb ~/.irbrc.rb
 ln -sf ~/code/dotfiles/pryrc.rb ~/.pryrc
 ln -sf ~/code/dotfiles/rspec ~/.rspec
 ln -sf ~/code/dotfiles/rubocop.yml ~/.rubocop.yml
-rm -rf ~/.oh-my-zsh/custom/themes/
-ln -sf ~/code/dotfiles/zsh/themes ~/.oh-my-zsh/custom
+ln -sf ~/code/dotfiles/zsh/themes/bolso.zsh-theme ~/.oh-my-zsh/custom/themes/bolso.zsh-theme
 ln -sf ~/code/dotfiles/zshrc.sh ~/.zshrc
 
 gem install specific_install
