c3ecaec1d34bae8f94125a971fa8d230252577fa
David Runger <davidjrunger@gmail.com>
2023-11-01 01:34:39 -0500

    Delete what is not needed sice we're using davidrunger-cl dotfiles

--- irbrc.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# NOTE! Because of the `.rb` extension, IRB doesn't automatically load this. That's what we want,
-# because, if IRB does automatically load it, then IRB doesn't respect project-local settings.
-# Instead, create a project-local `.irbrc` file, and in it put `load '/Users/david/.irbrc.rb'`.
-
-require_relative './utils/ruby/monkeypatch_repl.rb'
