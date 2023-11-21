7c0064c9ee0de4a65a4f664bc28aaf91dff9a14f
David Runger <davidjrunger@gmail.com>
2023-11-20 23:53:10 -0600

    Update dotfiles references to dotfiles-personal

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ln -sf ~/code/dotfiles/initializers/z.rb ~/code/david_runger/config/initializers/z.rb
-ln -sf ~/code/dotfiles/zprofile.sh ~/.zprofile
+ln -sf ~/code/dotfiles-personal/initializers/z.rb ~/code/david_runger/config/initializers/z.rb
+ln -sf ~/code/dotfiles-personal/zprofile.sh ~/.zprofile
 
 git config core.hookspath ~/code/dotfiles/githooks/dotfiles
 
