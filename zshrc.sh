059fae9d5f5cf61366aa4433803e945e63a9b59d
David Runger <davidjrunger@gmail.com>
2023-12-22 02:34:55 -0600

    [zshrc] Remove nvm
    I think I'm going to start managing node via asdf.

--- zshrc.sh
+++ zshrc.sh
@@ -5,10 +5,4 @@ export PATH="$HOME/code/dotfiles-personal/bin:$PATH"
 
 export NOTES_DIRECTORY="$HOME/Sync/notes"
 
-export NVM_DIR="/Users/david/.nvm" # must be an absolute path for some reason; can't start with `~/`
-# Load nvm lazily! Modified slightly from:
-# add our default nvm node (`nvm alias default v10.16.0`) to path without loading nvm
-# alias `nvm` to this one liner lazy load of the normal nvm script
-alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
-
 export EDITOR=subl
