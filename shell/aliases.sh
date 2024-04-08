dc2f3053699a33e84d854ab952f29607ee52732a
David Runger <davidjrunger@gmail.com>
2024-04-08 17:42:27 -0500

    [aliases] Only use /bin/ls (not eza) on non-Linux setups
    This is because I cannot install eza on my old MacBook Air.

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,3 +1,6 @@
 alias david="cd ~/code/david_runger"
-unalias ls > /dev/null 2>&1 || true
-alias ls="/bin/ls"
+
+if [ -z $LINUX ]; then
+  unalias ls > /dev/null 2>&1 || true
+  alias ls="/bin/ls"
+fi
