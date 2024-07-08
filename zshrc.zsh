f8e9773c458f8977c18144ed9333ce87a2d1c198
David Runger <davidjrunger@gmail.com>
2024-07-08 16:28:55 -0500

    [zshrc] Export COMMIT_TO_MAIN_REGEX
    And delete `bin/verify-on-ok-branch`, which we will use instead the
    version in `dotfiles`, which will reference the `COMMIT_TO_MAIN_REGEX`.

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,5 +1,10 @@
 . "$HOME/.asdf/asdf.sh"
 
+export COMMIT_TO_MAIN_REGEX="$HOME/("\
+"code/(blog|dotfiles-personal|dotfiles|github-jira-links)|"\
+"Sync/bin"\
+")/?$"
+
 export NOTES_DIRECTORY="$HOME/Filen/notes"
 
 export EDITOR=code
