04cff1c5b9d44772267adde35b9a0e3d8c543e8a
David Runger <davidjrunger@gmail.com>
2024-08-07 15:48:42 -0500

    [zprofile] Delete zprofile.zsh
    This was for Sublime, but I don't use Sublime anymore.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 ln -sf ~/code/dotfiles-personal/initializers/z.rb ~/code/david_runger/config/initializers/z.rb
-ln -sf ~/code/dotfiles-personal/zprofile.zsh ~/.zprofile
 
 git config core.hookspath ~/code/dotfiles/githooks/dotfiles
 
--- zprofile.zsh
+++
@@ -1,3 +0,0 @@
-# This file seems to be needed for Sublime Text to have access to the desired $PATH.
-# https://lsp.sublimetext.io/troubleshooting/#updating-the-path-used-by-lsp-servers
-export PATH="$PATH"
