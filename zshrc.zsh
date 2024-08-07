d5a3bcadb516f48e72f97ff763cbe681c925b8cc
David Runger <davidjrunger@gmail.com>
2024-08-07 17:00:46 -0500

    [zshrc] Delete exported COMMIT_TO_MAIN_REGEX
    Instead, going forward, we will rely on `runger-config` to permit
    pushing directly to a repo's main branch. (That's why this commit also
    adds a `.runger-config.yml` file permitting commits directly to `main`.)

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,8 +1,3 @@
-export COMMIT_TO_MAIN_REGEX="$HOME/("\
-"code/(blog|dotfiles-personal|dotfiles|github-jira-links)|"\
-"Sync/bin"\
-")/?$"
-
 export NOTES_DIRECTORY="$HOME/Filen/notes"
 
 export EDITOR=editor
